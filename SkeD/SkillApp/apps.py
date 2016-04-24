5f9e1c2e87f2c21f5283c1b2848f74ab5af0b952
vaibhavsoni11 <vaibhavsoni411@gmail.com>
2016-04-24 17:35:45 -0400

    Initial Check-in
    Scafolded project

---
+++ SkeD/SkillApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SkillAppConfig(AppConfig):
+    name = 'SkillApp'
---
+++ SkeD/SkillApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def findSkill(request):
+    """Renders the Find Skill page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
