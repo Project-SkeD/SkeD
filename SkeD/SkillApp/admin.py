760a8a805d0de30eb50f102d46f84c50636630d4
vaibhavsoni11 <vaibhavsoni411@gmail.com>
2016-04-30 20:43:23 -0400

    Missed Check-in

--- SkeD/SkillApp/admin.py
+++ SkeD/SkillApp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from SkillApp.models import *
 
-# Register your models here.
+class ApplicantAdmin(admin.ModelAdmin):
+    pass
+
+class CityAdmin(admin.ModelAdmin):
+    pass
+
+class JobRoleAdmin(admin.ModelAdmin):
+    pass
+
+class QualificationAdmin(admin.ModelAdmin):
+    pass
+
+class ExperienceAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Applicant,ApplicantAdmin)
+admin.site.register(City,CityAdmin)
+admin.site.register(JobRole, JobRoleAdmin)
+admin.site.register(Qualification,QualificationAdmin)
+admin.site.register(Experience,ExperienceAdmin)
--- SkeD/SkillApp/models.py
+++ SkeD/SkillApp/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+class Applicant(models.Model):
+    Applicant_Id = models.AutoField(primary_key=True)
+    FirstName = models.CharField(max_length = 30)
+    LastName = models.CharField(max_length = 30)
+    DateOfBirth = models.DateField()
+    City = models.ForeignKey("City")
+    Qualification = models.CharField(max_length = 50)
+    Job = models.ForeignKey("JobRole")
+    #Skills = models.ForeignKey("Skills")
+    Experience = models.IntegerField()
+    ExpectedSalary = models.IntegerField()
+    Languages = models.CharField(max_length = 30)
+    Verified_By = models.CharField(max_length = 50)
+    
+    def __unicode__(self):
+        return str(self.FirstName) + " " +str(self.LastName)
+
+class City(models.Model):
+    City_Id = models.AutoField(primary_key=True)
+    CityName = models.CharField(max_length = 30)
+    State = models.CharField(max_length = 30)
+
+    def __unicode__(self):
+        return str(self.CityName)
+
+class JobRole(models.Model):
+    JobRole_Id = models.AutoField(primary_key=True)
+    Jobtype = models.CharField(max_length = 30)
+    IndustryName = models.CharField(max_length = 30)
+    
+    def __unicode__(self):
+        return str(self.Jobtype)
+
+class Qualification(models.Model):
+    InstitutionName = models.CharField(max_length = 50)
+    EducationLevel = models.CharField(max_length = 10)
+    StartDate = models.DateField()
+    EndDate = models.DateField()
+        
+    def __unicode__(self):
+        return str(self.InstitutionName)
+
+class Experience(models.Model):
+    Applicant_Name = models.ForeignKey("Applicant")
+    EmployerName = models.CharField(max_length = 50)
+    StartDate = models.DateField()
+    EndDate = models.DateField()
+    
+    def __unicode__(self):
+        return str(self.EmployerName)
